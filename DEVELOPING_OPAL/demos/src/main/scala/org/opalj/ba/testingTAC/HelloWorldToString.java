e1807032486d421e13bd19ffa3eb1b934fe5bdc5
S. Aranda-Simbron <sofia.arandasimbron@stud.tu-darmstadt.de>
2024-05-21 00:34:16 +0200

    updated testclass

--- DEVELOPING_OPAL/demos/src/main/scala/org/opalj/ba/testingTAC/HelloWorldToString.java
+++ DEVELOPING_OPAL/demos/src/main/scala/org/opalj/ba/testingTAC/HelloWorldToString.java
@@ -1,14 +1,36 @@
 package org.opalj.ba.testingTAC;
 
+import java.util.Scanner;
+
 public class HelloWorldToString {
 
     public static void main(String[] args) {
-        int x = 0;
-        int y = 2;
-        boolean b = (x % y) == 0;
-        boolean b2 = !b;
-        if(b || b2)
-        System.out.println("Hello World!");
+        String s1 = "Please enter a number.";
+        System.out.println(s1);
+        int a = s1.length();
+        String s2 = "Please enter another number.";
+        System.out.println(s2);
+        int b = s2.length();
+        int result = a + b;
+        boolean bool = true;
+        for(int i = 0; i < 10; i++){
+            if(result < i){
+                System.out.println("less than 5");
+            }else if( result == i){
+                System.out.println("zeroooo");
+                foo(bool);
+            }else{
+                System.out.println("moreee");
+            }
+            foo(bool);
+        }
+    }
+
+    public static boolean foo(boolean b){
+        for(int i = 0; i < 2; i++){
+            System.out.println("hello");
+        }
+        return !b;
     }
 }
 
